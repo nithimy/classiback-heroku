143d8e79fe934b6371c1bcb131930151bab3fa60
nithimy <57937879+nithimy@users.noreply.github.com>
2021-05-26 23:01:03 +0700

    update

--- src/server.js
+++ src/server.js
@@ -156,6 +156,6 @@ app.use(errHandler);
 // init all web routes
 initWebRoutes(app);
 
-let port = process.env.PORT || 5000;
-app.listen(process.env.PORT || 5000, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
+let port = process.env.port || '5000';
+app.listen(process.env.port || '5000', () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
 // app.listen(3000, '192.168.2.34', () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
