b3bd13a5c0d7b894e764c15661928f7f31944421
nithimy <57937879+nithimy@users.noreply.github.com>
2021-05-27 03:57:36 +0700

    Update DBConnection.js

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -1,20 +1,30 @@
 require('dotenv').config();
 import mysql from "mysql2";
 
-let connection = mysql.createConnection({
-    host: 'app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com',
-    user: 'classi',
-    password: 'pocvb9zaolz42knu',
-    database: 'classi',
-    port: 25060,
-    ssl: true,
-});
+// let connection = mysql.createConnection({
+//     host: 'app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com',
+//     user: 'classi',
+//     password: 'pocvb9zaolz42knu',
+//     database: 'classi',
+//     port: 25060,
+//     ssl: true,
+// });
 
-connection.connect(function(err) {
-    console.log(err);
-    if (err) throw err;
-    console.log("Database connected!");
-});
+var connection = mysql.createConnection({
+    host: "app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com",
+    user: "classi",
+    password: "pocvb9zaolz42knu",
+    database: "classi",
+    multipleStatements:true,
+    port: 25060,
+    queryTimeout: 6000,
+    connectTimeout: 60000,
+    ssl: true,
+    });
+    
+    con.connect(function (err){
+    if(err) throw err;
+    });
 
 module.exports = connection;
 
