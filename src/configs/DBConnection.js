bcd7b6158545104b74cf871b1af9f31aeaa3e61f
nithimy <57937879+nithimy@users.noreply.github.com>
2021-05-27 12:17:53 +0700

    Update DBConnection.js

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import mysql from "mysql2";
 
+const fs = require('fs')
+const path = require('path')
 // let connection = mysql.createConnection({
 //     host: 'app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com',
 //     user: 'classi',
@@ -9,19 +11,43 @@ import mysql from "mysql2";
 //     port: 25060,
 //     ssl: true,
 // });
-
-var connection = mysql.createConnection({
-    host: "app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com",
-    user: "classi",
-    password: "pocvb9zaolz42knu",
-    database: "class",
-    multipleStatements:true,
+const MYSQL_CONF = {
+    username: 'b09204a1f66b9f',
+    password: 'pocvb9zaolz42knu',
+    database: 'class',
+    host: 'db-mysql-v1-29254-do-user-7904424-0.a.db.ondigitalocean.com',
     port: 25060,
+    dialect: 'mysql',
     connectTimeout: 60000,
-    });
-    
-    connection.connect(function (err){
-    if(err) throw err;
-    });
+    dialectOptions: {
+        ssl: {
+            ssl: true,
+            cert: fs.readFileSync(path.resolve(__dirname, 'ca-certificate.crt')).toString()
+        }
+    }
+}
 
-module.exports = connection;
+// var connection = mysql.createConnection({
+//     host: "app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com",
+//     username: "classi",
+//     password: "pocvb9zaolz42knu",
+//     database: "class",
+//     multipleStatements:true,
+//     port: 25060,
+//     connectTimeout: 60000,
+//     });
+    
+//     connection.connect(function (err){
+//     if(err) throw err;
+//     });
+
+// module.exports = connection;
+const con = mysql.createConnection(MYSQL_CONF)
+con.connect((err, result) => {
+    if (err) {
+        throw new Error(err)
+        return
+    } else {
+        console.log('Successfully connecting with the database')
+    }
+});
