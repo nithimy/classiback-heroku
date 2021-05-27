1ea905b8cc32b7bb463eecc30268d06641a7aa8d
nithimy <57937879+nithimy@users.noreply.github.com>
2021-05-27 12:01:42 +0700

    Update DBConnection.js

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -14,15 +14,14 @@ var connection = mysql.createConnection({
     host: "app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com",
     user: "classi",
     password: "pocvb9zaolz42knu",
-    database: "classi",
+    database: "class",
     multipleStatements:true,
     port: 25060,
     connectTimeout: 60000,
     });
     
-    con.connect(function (err){
+    connection.connect(function (err){
     if(err) throw err;
     });
 
 module.exports = connection;
-
