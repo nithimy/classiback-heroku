b270f36245e5294a85b6dc368d0f133a2eb2e7fb
nithimy <57937879+nithimy@users.noreply.github.com>
2021-05-28 22:35:16 +0700

    Update DBConnection.js

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -1,53 +1,19 @@
-require('dotenv').config();
+// require('dotenv').config();
 import mysql from "mysql2";
+const mysql = require("mysql");
 
-const fs = require('fs')
-const path = require('path')
-// let connection = mysql.createConnection({
-//     host: 'app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com',
-//     user: 'classi',
-//     password: 'pocvb9zaolz42knu',
-//     database: 'classi',
-//     port: 25060,
-//     ssl: true,
-// });
-const MYSQL_CONF = {
-    username: 'b09204a1f66b9f',
-    password: '06e3b8b',
-    database: 'class',
-    host: 'dus-cdbr-east-03.cleardb.com',
-    port: 25060,
-    dialect: 'mysql',
-    connectTimeout: 60000,
-    dialectOptions: {
-        ssl: {
-            ssl: true,
-            cert: fs.readFileSync(path.resolve(__dirname, 'ca-certificate.crt')).toString()
-        }
-    }
-}
-
-// var connection = mysql.createConnection({
-//     host: "app-709a4f14-3b21-4aee-889c-a3984e00b711-do-user-9291773-0.b.db.ondigitalocean.com",
-//     username: "classi",
-//     password: "pocvb9zaolz42knu",
-//     database: "class",
-//     multipleStatements:true,
-//     port: 25060,
-//     connectTimeout: 60000,
-//     });
-    
-//     connection.connect(function (err){
-//     if(err) throw err;
-//     });
-
-// module.exports = connection;
-const con = mysql.createConnection(MYSQL_CONF)
-con.connect((err, result) => {
-    if (err) {
-        throw new Error(err)
-        return
-    } else {
-        console.log('Successfully connecting with the database')
-    }
+let connection = mysql.createConnection({
+    host: "us-cdbr-east-03.cleardb.com",
+    user: "b57d3e74fb8377",
+    password: "7fad4843",
+    database: "heroku_68878ccdd651069"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected!");
+});
+
+module.exports = connection;
+
+// mysql://b57d3e74fb8377:7fad4843@us-cdbr-east-03.cleardb.com/heroku_68878ccdd651069?reconnect=true
